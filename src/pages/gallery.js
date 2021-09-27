35d3d19c9bb901437ce3410d1d832df5b21dde7f
frostpebble <frostpebble@gmail.com>
2021-09-27 19:33:17 +0200

    put commissions in gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,45 +4,67 @@ import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import Layout from "../components/layout"
 
-const Gallery = ({ data }) => (
+const Gallery = ({ data }) => {
+  var fullImage;
+  var showFullview = true;
+  function fullview(image) {
+    console.log("fullview");
+    fullImage = image;
+    showFullview = true;
+  }
+
+  return (
   <Layout>
     <Masonry className="showcase">
-      {data.allDatoCmsWork.edges.map(({ node: work }) => (
-        <div key={work.id} className="showcase__item">
+      {data.commissions.edges.map(({ node: comm }) => (
+        <div key={comm.id} className="showcase__item">
           <figure className="card">
-            <Link to={`/works/${work.slug}`} className="card__image">
+{/*             <Link to={`/works/${work.slug}`} className="card__image">
               <Img fluid={work.coverImage.fluid} />
-            </Link>
+            </Link> */}
+{/*             <div className="card__image" onClick={() => {fullview(comm.fluid)}}>
+              <Img fluid={comm.fluid} />
+            </div> */}
+            <a href={comm.url} target="_blank">
+            <div className="card__image">
+              <Img fluid={comm.fluid} />
+            </div>
+            </a>
             <figcaption className="card__caption">
-              <h6 className="card__title">
-                <Link to={`/works/${work.slug}`}>{work.title}</Link>
-              </h6>
+{/*               <h6 className="card__title">
+                <Link to={comm.notes}>Artist: {comm.author}</Link>
+              </h6> */}
               <div className="card__description">
-                <p>{work.excerpt}</p>
+              <a href={comm.notes}>Artist: {comm.author}</a>
               </div>
             </figcaption>
           </figure>
         </div>
       ))}
+      { (fullImage && showFullview) && <div className="image__full">
+        <Img fluid={fullImage} />
+        image goes here
+      </div> }
     </Masonry>
   </Layout>
-)
+)}
 
 export default Gallery
 
+
+
 export const query = graphql`
   query GalleryQuery {
-    allDatoCmsWork(sort: { fields: [position], order: ASC }) {
+    commissions: allDatoCmsAsset(filter: { tags: { in: "commission" } }) {
       edges {
         node {
           id
-          title
-          slug
-          excerpt
-          coverImage {
-            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
+          author
+          tags
+          url
+          notes
+          fluid(maxWidth: 1000, imgixParams: { fm: "jpg", auto: "compress" }) {
               ...GatsbyDatoCmsSizes
-            }
           }
         }
       }
